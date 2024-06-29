1493fecb870a7b1190d4bde2af8a8f19e8fc691e
Marcelo Augusto Borrago <marceloborrago@gmail.com>
2024-06-29 13:08:44 -0300

    feat: Adicionado Material UI

--- frontend/clientes/src/routes/index.tsx
+++ frontend/clientes/src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<p>Página inicial</p>} />
+            <Route path="/" element={<Button>Teste</Button>} />
 
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
